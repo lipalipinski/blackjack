39598e6475bc11279030b324449c8618140cc6df
JL <lipabass@gmail.com>
2022-02-22 01:40:25 +0100

    First functional version

--- blackjack.py
+++ blackjack.py
@@ -4,12 +4,15 @@ by JL 2022
 """
 import random
 
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Ace': 0, 'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6,
-          'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10,
-          'Queen': 10, 'King': 10}
+suits = {'H': '\u2661', 'D': '\u2662', 'S': '\u2664', 'C': '\u2667'}
+rank_values = {'A': 0, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+               '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10,
+               'Q': 10, 'K': 10}
+
+
+def clr_scr():
+    """clear console with 100 blank lines"""
+    print('\n'*100)
 
 
 class Card():
@@ -20,41 +23,376 @@ class Card():
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        self.value = values[rank]
+        self.value = rank_values[rank]
 
     def __str__(self):
-        return self.rank + ' of ' + self.suit
+        return ' '.join([self.rank, suits[self.suit]])
 
 
-class Deck():
+class Decks():
     '''
-    52 cards deck
+    given number of 52 cards decks (shuffled)
+    .cut_card - when fewer cards left, get new decks
     '''
 
-    def __init__(self):
-        self.all_cards = []
-        for suit in suits:
-            for rank in ranks:
-                self.all_cards.append(Card(suit, rank))
+    def __init__(self, number=1):
+        """
+        number - number of decks
+        """
+        self.decks_number = number
+        self.cut_card = random.randint(15, 25)
+        self.fresh_cards = []
+        self.used_cards = []
+
+        for _ in range(number):
+            for suit in suits.keys():
+                for rank in rank_values.keys():
+                    self.fresh_cards.append(Card(suit, rank))
+        self.shuffle()
 
     def shuffle(self):
         '''
         shuffle the deck
         '''
-        random.shuffle(self.all_cards)
+        random.shuffle(self.fresh_cards)
 
     def deal(self):
         '''
-        pop one card from the deck
+        pop one card from the deck.
+        if cut card met and no cards on the table, join used and shuffle
+        if cards on the table shufle used and pop from used
         '''
-        return self.all_cards.pop()
+        if len(self.fresh_cards) < self.cut_card:
+            # if no cards on the table
+            if len(self.fresh_cards)\
+             + len(self.used_cards) == 52 * self.decks_number:
+                print('Cut card: shuffling the deck.')
+                self.fresh_cards.extend(self.used_cards)
+                self.shuffle()
+            # some cards on the table
+            elif len(self.fresh_cards) == 0:
+                random.shuffle(self.used_cards)
+                return self.used_cards.pop()
+        return self.fresh_cards.pop()
+
+    def return_cards(self, cards):
+        """
+        put card(s) into .used_cards
+        """
+        if isinstance(cards, list):
+            self.used_cards.extend(cards)
+        else:
+            self.used_cards.append(cards)
+
+
+class Player():
+    """
+    human blackjack player
+    """
+
+    def __init__(self, name, account):
+        self.name = name
+        self.account = account
+        self.bet_ammount = 0
+        self.decision_result = None
+
+    def __str__(self):
+        return f'Player {self.name}: {self.account}$'
+
+    def bet(self, min_bet):
+        """
+        ask player for a bet.
+        min_bet - minimal bet
+        return False when user wants to quit
+        """
+        if self.account < min_bet:
+            return False
+        else:
+            value = 0
+            while True:
+                try:
+                    value = input(f'{self.name} type your bet value (bank: {self.account}$)')
+                    if value == 'Q':
+                        return False
+                    value = int(value)
+                except ValueError:
+                    print('Bet value should be a number.')
+                    continue
+
+                if value in range(min_bet, self.account):
+                    break
+                else:
+                    print(f'Bet value should be between {min_bet} and {self.account}')
+            self.account -= value
+            self.bet_ammount = value
+
+    def win(self, ammount):
+        """
+        add ammount to account
+        """
+        self.account += ammount
+
+    def decision(self):
+        """
+        returns 'h' (hit) / 's' (stand) / False on user quit
+        """
+
+        self.decision_result = None
+        decision = ' '
+        while True:
+            decision = input('Type [h] for hit or [s] for stand: ')
+            if decision in ['h', 's']:
+                self.decision_result = decision
+                break
+            elif decision == 'Q':
+                return False
+
+
+class Table():
+    """
+    main table. hold players, cards and display
+    """
+
+    def __init__(self, decks_num):
+        """decks_num - number of decks"""
+        self.deck = Decks(decks_num)
+        self.player1 = Player('Player1', 1000)
+        self.dealers_cards = []
+        self.player_cards = []
+        self.round_counter = 0
+
+    def hand_value(self, cards):
+        """
+        return blackjack hand value. Determine Ace to be 1 or 11.
+        If value == 21 return True, if over return False
+        """
+        hand_value = 0
+        for card in cards:
+            if card.value != 0:
+                hand_value += card.value
+        for card in cards:
+            if card.value == 0 and hand_value > 10:
+                hand_value += 1
+            elif card.value == 0 and hand_value <= 10:
+                hand_value += 11
+        if hand_value > 21:
+            return False
+        elif hand_value == 21:
+            return True
+        else:
+            return hand_value
+
+    def new_game(self):
+        """initial deal"""
+        for _ in [0, 1]:
+            self.dealers_cards.append(self.deck.deal())
+            self.player_cards.append(self.deck.deal())
+
+    def dealers_move(self):
+        """
+        return True if dealer's 21,
+        False if dealer busts or value 17-21 
+        """
+
+        while True:
+            
+            hand = self.hand_value(self.dealers_cards)
+            if hand is True:
+                # dealer's 21
+                return True
+            
+            elif hand is False:
+                # dealer busts
+                return False
+
+            elif hand < 17:
+                # dealer takes card
+                self.dealers_cards.append(self.deck.deal())
+
+            else:
+                # dealer's hand
+                return hand
+
+    def player_hit(self):
+        """player takes a card"""
+        self.player_cards.append(self.deck.deal())
+
+    def is_blackjack(self):
+        """
+        check if blackjack (after first deal).
+        return True if blackjack, False if not
+        """
+        player_result = self.hand_value(self.player_cards)
+        dealer_result = self.hand_value(self.dealers_cards)
+
+        # Tie blackjack
+        if player_result == dealer_result is True:
+            print('Blackjack tie!')
+            self.player1.win(self.player1.bet_ammount)
+            return True
+        
+        # Player blackjack
+        elif dealer_result != player_result is True:
+            print('Player has blackjack!')
+            price = int(self.player1.bet_ammount * 5 / 2)
+            self.player1.win(price)
+            return True
+        
+        # dealer blackjack
+        elif player_result != dealer_result is True:
+            print('Dealer has blackjack!')
+            return True
+
+        # no blackjack
+        return False
+    
+    def round_result(self):
+        """
+        determine round results
+        """
+        player_result = self.hand_value(self.player_cards)
+        dealer_result = self.hand_value(self.dealers_cards)
+        print(f'Player {player_result}\nDealer {dealer_result}')
+        
+        # Tie
+        if player_result == dealer_result is not False:
+            print('Tie!')
+            self.player1.win(self.player1.bet_ammount)
+
+        # Player 21 dealer not
+        elif dealer_result != player_result is True:
+            print('Player wins!')
+            price = self.player1.bet_ammount * 2
+            self.player1.win(price)
+        
+        # dealer 21 player not
+        elif player_result != dealer_result is True:
+            print('Player lost')
+
+        # player wins
+        elif player_result > dealer_result:
+            print('Player wins!')
+            price = self.player1.bet_ammount * 2
+            self.player1.win(price)
+
+        # player lost
+        elif player_result < dealer_result:
+            print('Player lost')
+
+    def end_round(self):
+        """return cards to deck"""
+        self.deck.return_cards(self.dealers_cards)
+        self.deck.return_cards(self.player_cards)
+        self.dealers_cards = []
+        self.player_cards = []
+        self.round_counter += 1
+
+    def display(self, show_all=False):
+        """
+        display game. show_all - if False show only first dealer's card
+        """
+
+        clr_scr()
+        print(f'Round: {self.round_counter}')
+        print("Dealer's cards:")
+        if show_all is True:
+            for card in self.dealers_cards:
+                print(card)
+        else:
+            print(self.dealers_cards[0])
+            for card in self.dealers_cards[1:]:
+                print('[]')
+        print(f"Player's cards ({self.hand_value(self.player_cards)}):")
+        for card in self.player_cards:
+            print(card)
+        print(f"Player's bank: {self.player1.account}$")
+        print(f"Player's bet: {self.player1.bet_ammount}$")
+
+
+class Menu():
+    """
+    main menu.
+    state 0 - show menu, 1 - new game, 2 - quit game
+    """
+
+    def __init__(self):
+        self.state = 0
+
+    def display_menu(self):
+        """display main menu"""
+
+        clr_scr()
+        print('Blackjack\nJL 2022\n')
+        print('1 - new game\n2 - quit')
+        while True:
+            try:
+                inp = int(input())
+            except ValueError:
+                print('Incorrect value')
+            if inp in [1, 2]:
+                self.state = inp
+                break
 
 
 def main():
     """
     main function
     """
-    pass
+
+    main_menu = Menu()
+    while main_menu.state != 2:
+
+        # display menu
+        if main_menu.state == 0:
+            main_menu.display_menu()
+
+        # new game
+        elif main_menu.state == 1:
+            
+            table = Table(1)
+
+            # game loop
+            while True:
+                table.new_game()
+
+                # player bets
+                if table.player1.bet(50) is False:
+                    main_menu.state = 0
+                    break
+
+                table.display()
+                
+                # check if blackjack
+                if table.is_blackjack() is True:
+                    # if blackjack start over
+                    table.end_round()
+                    continue
+
+                # player's decision
+                while True:
+
+                    # player quits game
+                    if table.player1.decision() is False:
+                        main_menu.state = 0
+                        break
+                    
+                    # player hits - dealer's turn
+                    if table.player1.decision_result == 'h':
+                        table.player_hit()
+                        table.display()
+
+                    # player stands
+                    elif table.player1.decision_result == 's':
+                        break
+                    
+                    # player bust
+                    if isinstance(table.hand_value(table.player_cards), bool):
+                        break
+    
+                table.dealers_move()
+                table.display(True)
+                table.round_result()
+                table.end_round()
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,14 +1,18 @@
+"""
+blackjack tests
+"""
+
 import unittest
 import blackjack
 
 
 class TestCard(unittest.TestCase):
-    '''tests for war.Card class'''
+    '''tests for blackjack.Card class'''
 
     def test_card_value(self):
         '''getting value from dict'''
 
-        for rank, value in blackjack.values.items():
+        for rank, value in blackjack.rank_values.items():
             test_card = blackjack.Card('Hearts', rank)
             result = test_card.value
             self.assertEqual(result, value)
@@ -16,29 +20,210 @@ class TestCard(unittest.TestCase):
     def test_string(self):
         '''casting class as a string'''
 
-        suit = 'Hearts'
-        rank = 'Ten'
+        suit = 'H'
+        rank = '10'
         result = str(blackjack.Card(suit, rank))
-        self.assertEqual(result, 'Ten of Hearts')
+        self.assertEqual(result, '10 \u2661')
 
 
-class TestDeck(unittest.TestCase):
-    '''tests for war.Deck class'''
+class TestDecks(unittest.TestCase):
+    '''tests for blackjack.Deck class'''
+
+    def test_def_cards_quantity(self):
+        '''are there 52 cards in deck'''
+        test_deck = blackjack.Decks()
+        self.assertEqual(len(test_deck.fresh_cards), 52)
 
     def test_cards_quantity(self):
         '''are there 52 cards in deck'''
-        test_deck = blackjack.Deck()
-        self.assertEqual(len(test_deck.all_cards), 52)
+        test_deck = blackjack.Decks(2)
+        self.assertEqual(len(test_deck.fresh_cards), 2*52)
 
     def test_shuffle(self):
         '''is shuffle shuffling a deck'''
 
-        test_deck = blackjack.Deck()
-        not_shuffled = list(test_deck.all_cards)
+        test_deck = blackjack.Decks()
+        not_shuffled = list(test_deck.fresh_cards)
         test_deck.shuffle()
-        self.assertNotEqual(not_shuffled, test_deck.all_cards)
+        self.assertNotEqual(not_shuffled, test_deck.fresh_cards)
+
+    def test_return_cards_list(self):
+        """return list of cards"""
+        lista = [blackjack.Card('Hearts', '2'),
+                 blackjack.Card('Hearts', '3')]
+        test_deck = blackjack.Decks()
+        test_deck.return_cards(lista)
+        self.assertEqual(test_deck.used_cards, lista)
+
+    def test_return_cards_card(self):
+        """return single card"""
+        karta = blackjack.Card('Hearts', '2')
+        test_deck = blackjack.Decks()
+        test_deck.return_cards(karta)
+        self.assertEqual(test_deck.used_cards[0], karta)
+
+    def test_deal_all_cards(self):
+        """deal all cards to table (no returns)"""
+        test_deck = blackjack.Decks()
+        for _ in range(52):
+            karta = test_deck.deal()
+            self.assertTrue(isinstance(karta, blackjack.Card))
+        self.assertEqual(len(test_deck.fresh_cards), 0)
+
+    def test_deal_not_table(self):
+        """keep dealing after cut_card (no cards on table)"""
+        test_deck = blackjack.Decks()
+        for _ in range(53):
+            karta = test_deck.deal()
+            test_deck.return_cards(karta)
+            self.assertTrue(isinstance(karta, blackjack.Card))
+
+    def test_deal_cards_on_table(self):
+        """keep dealing after cut_card (cards on table)"""
+        test_deck = blackjack.Decks()
+        for _ in range(52):
+            karta = test_deck.deal()
+            test_deck.return_cards(karta)
+            self.assertTrue(isinstance(karta, blackjack.Card))
+        for _ in range(52):
+            karta = test_deck.deal()
+            self.assertTrue(isinstance(karta, blackjack.Card))
+
+
+class TestTable(unittest.TestCase):
+    """test blackjack.Table class"""
+
+    # .new_game
+    def test_new_game(self):
+        """take cards from deck"""
+        test_table = blackjack.Table(1)
+        test_table.new_game()
+        self.assertEqual(len(test_table.deck.fresh_cards), 48)
+
+    def test_new_game_2(self):
+        """cards on the table"""
+        test_table = blackjack.Table(1)
+        test_table.new_game()
+        self.assertEqual(len(test_table.player_cards), 2)
+        self.assertEqual(len(test_table.dealers_cards), 2)
+
+    # .end_round
+    def test_end_round(self):
+        """return cards to deck"""
+        test_table = blackjack.Table(1)
+        test_table.new_game()
+        test_table.end_round()
+        self.assertEqual(len(test_table.deck.used_cards), 4)
+
+    # .hand_value
+    def test_hand_value_no_aces_1(self):
+        """under 21"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', '2'), blackjack.Card('H', 'J')]
+        self.assertEqual(test_table.hand_value(card_set), 12)
+
+    def test_hand_value_no_aces_2(self):
+        """over 21"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', 'K'), blackjack.Card('H', 'J'),
+                    blackjack.Card('H', '2')]
+        self.assertFalse(test_table.hand_value(card_set))
+
+    def test_hand_value_no_aces_3(self):
+        """21"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', 'K'), blackjack.Card('H', '6'),
+                    blackjack.Card('H', '5')]
+        self.assertTrue(test_table.hand_value(card_set))
+
+    def test_hand_value_with_aces_1(self):
+        """under 21"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', '2'), blackjack.Card('H', 'A')]
+        self.assertEqual(test_table.hand_value(card_set), 13)
+
+    def test_hand_value_with_aces_2(self):
+        """16 + ace"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', 'A'), blackjack.Card('H', '6'),
+                    blackjack.Card('H', 'K')]
+        self.assertEqual(test_table.hand_value(card_set), 17)
+
+    def test_hand_value_with_aces_3(self):
+        """10 + ace == 21"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', '10'), blackjack.Card('H', 'A')]
+        self.assertTrue(test_table.hand_value(card_set))
+
+    def test_hand_value_with_aces_4(self):
+        """two aces"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', 'A'), blackjack.Card('H', 'A')]
+        self.assertEqual(test_table.hand_value(card_set), 12)
+
+    def test_hand_value_with_aces_5(self):
+        """four aces"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', 'A'), blackjack.Card('H', 'A'),
+                    blackjack.Card('H', 'A'), blackjack.Card('H', 'A')]
+        self.assertEqual(test_table.hand_value(card_set), 14)
+
+    def test_hand_value_with_aces_6(self):
+        """6 + ace"""
+        test_table = blackjack.Table(1)
+        card_set = [blackjack.Card('H', '6'), blackjack.Card('H', 'A')]
+        self.assertEqual(test_table.hand_value(card_set), 17)
+
+    # dealers move
+    def test_dealers_move_1(self):
+        """dealer's blackjack"""
+        test_table = blackjack.Table(1)
+        test_table.dealers_cards = [blackjack.Card('H', 'K'),
+                                    blackjack.Card('H', 'A')]
+        self.assertTrue(test_table.dealers_move())
+
+    def test_dealers_move_2(self):
+        """dealer busts"""
+        test_table = blackjack.Table(1)
+        test_table.dealers_cards = [blackjack.Card('H', '8'),
+                                    blackjack.Card('H', '8'),
+                                    blackjack.Card('H', '6')]
+        self.assertFalse(test_table.dealers_move())
+
+    def test_dealers_move_3(self):
+        """input less than 17"""
+        test_table = blackjack.Table(1)
+        test_table.dealers_cards = [blackjack.Card('H', '9'),
+                                    blackjack.Card('H', '9')]
+        result = test_table.dealers_move()
+        expected = [True, False]
+        expected.extend([x for x in range(17, 22)])
+        self.assertIn(result, expected)
+
+    def test_dealers_move_4(self):
+        """input in (17,21)"""
+        test_table = blackjack.Table(1)
+        test_table.dealers_cards = [blackjack.Card('H', '9'),
+                                    blackjack.Card('H', '9')]
+        self.assertEqual(test_table.dealers_move(), 18)
+
+    def test_player_hit(self):
+        """player takes card"""
+        test_table = blackjack.Table(1)
+        test_table.player_hit()
+        self.assertIsInstance(test_table.player_cards[0], blackjack.Card)
+
+
+
+class TestPlayer(unittest.TestCase):
+    """blackjack.Player class"""
+
+    def test_win(self):
+        """adding to account"""
+        test_player = blackjack.Player('x', 0)
+        test_player.win(10)
+        self.assertEqual(test_player.account, 10)
 
 
 if __name__ == '__main__':
     unittest.main()
-    
